f7e3b9b4b35f364432604a9213949ebd0be40e9d
RounakJoshi09 <rounakjsh783@gmail.com>
2024-01-01 23:54:39 +0530

    Multiply Added (#2)
    Co-authored-by: RounakBytes <144313548+RounakBytes@users.noreply.github.com>

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const add = (x, y) => {
   return x + y;
 };
 
+const multiply = (x, y) => {
+  return x * y;
+}
+
+console.log(add(5, 10));
+console.log(multiple(5, 25));
 const divide = (x, y) => {
   return x / y;
 };
